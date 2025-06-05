ae5d78f6fee15488d942d776adc27f50393dc000
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2025-06-05 09:38:44 -0500

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+@app.route('/')
+def index():
+    return "El servidor está en funcionamiento."
+
+
+def run():
+    app.run(host='0.0.0.0', port=8000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
