bab37358d52aebdb6f624cee886637ab88802fc3
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2025-06-05 10:08:48 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from threading import Thread
 import os
+from flask import Flask, render_template
+from threading import Thread
 
+app = Flask(__name__)
 
-app = Flask("")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 def run():
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    port = int(os.getenv("PORT", 8080))  # Railway usa una variable de entorno PORT
+    app.run(host="0.0.0.0", port=port)
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+def start_web():
+    thread = Thread(target=run)
+    thread.start()
