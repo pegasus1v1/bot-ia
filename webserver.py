a22e0aec4b82f55e79a771e72ab49f60a8e62c22
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2025-06-05 10:05:34 -0500

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from threading import Thread
+import os
+
 
 app = Flask("")
-@app.route('/')
-def index():
-    return "El servidor está en funcionamiento."
-
 
 def run():
-    app.run(host="0.0.0.0", port=os.getenv("PORT", 8000))
-
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
 
 def keep_alive():
     t = Thread(target=run)
